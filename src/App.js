b27c20d3a56fbee3f86d9ef46ffaabfd77d26151
dlirA01 ♡ <arild.edvin.andreassen@gmail.com>
2023-03-16 16:42:12 +0100

    did a few things

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
 import './css/main.css';
+import {Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-
+        <>
+          <Router>
+              <Routes>
+                <Route path="/" element="" />
+              </Routes>
+          </Router>
+        </>
     </div>
   );
 }
