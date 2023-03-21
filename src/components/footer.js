761a0198e5144c6b6c3b234d32e09f09ceea51de
dlirA01 ♡ <arild.edvin.andreassen@gmail.com>
2023-03-21 20:33:04 +0100

    done changes in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,9 +14,11 @@ export function Footer() {
                     <div className="footer-bottom-line-top"></div>
                     <div className="footer-bottom-line-bottom"></div>
                     <div className="footer-bottom-left">
-                        <svg className="footer-bottom-left-logo" viewBox="0 0 887.46 404.28">
-                            <text transform="translate(0 307.64)"><tspan x="0" y="0">C</tspan><tspan x="269.64" y="0">l</tspan><tspan x="339.49" y="0">o</tspan><tspan x="549.78" y="0">th</tspan></text>
-                        </svg>
+                        <NavLink className="footer-bottom-left-logo" to="/">
+                            <svg className="footer-bottom-left-logo-svg" viewBox="0 0 887.46 404.28">
+                                <text transform="translate(0 307.64)"><tspan x="0" y="0">C</tspan><tspan x="269.64" y="0">l</tspan><tspan x="339.49" y="0">o</tspan><tspan x="549.78" y="0">th</tspan></text>
+                            </svg>
+                        </NavLink>
                     </div>
                     <div className="footer-bottom-right">
                         <ul className="footer-bottom-right-items">
