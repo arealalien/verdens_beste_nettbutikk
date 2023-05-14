5a81037118c2f02e90b9b28671741133400a7bfb
dlirA01 ♡ <arild.edvin.andreassen@gmail.com>
2023-05-14 16:32:06 +0200

    did things

--- src/components/productList.js
+++ src/components/productList.js
@@ -10,7 +10,7 @@ export const clothes = [
     },
     {
         productId: 2,
-        productName: "Blue",
+        productName: "Blue Top",
         productPrice: 69.99,
         productImages: [
             "https://images.unsplash.com/photo-1599557852959-f1ebc5cc260e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
@@ -21,7 +21,7 @@ export const clothes = [
     },
     {
         productId: 3,
-        productName: "Flower Dress",
+        productName: "Black Top",
         productPrice: 69.99,
         productImages: [
             "https://images.unsplash.com/photo-1649219026888-aca032914261?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2030&q=80",
@@ -30,7 +30,7 @@ export const clothes = [
     },
     {
         productId: 4,
-        productName: "Cargo Pants",
+        productName: "Red Sweater",
         productPrice: 49.99,
         productImages: [
             "https://images.unsplash.com/photo-1549570652-97324981a6fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1970&q=80",
@@ -46,6 +46,49 @@ export const clothes = [
             "https://images.unsplash.com/photo-1566678124699-d693d419f72c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
             "https://images.unsplash.com/photo-1566678124687-0cf350e352c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
             "https://images.unsplash.com/photo-1566678124700-bdeaedbe18d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        ]
+    },
+    {
+        productId: 6,
+        productName: "Gray Suit",
+        productPrice: 129.99,
+        productImages: [
+            "https://images.unsplash.com/photo-1623685307246-19d9f5534ce6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+            "https://images.unsplash.com/photo-1621892944059-99493252c1f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+            "https://images.unsplash.com/photo-1622047583743-18ea2c0e7569?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+            "https://images.unsplash.com/photo-1621964275191-ccc01ef2134c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+            "https://images.unsplash.com/photo-1621892940075-de83b3ae7a00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+        ]
+    },
+    {
+        productId: 7,
+        productName: "Green",
+        productPrice: 39.99,
+        productImages: [
+            "https://images.unsplash.com/photo-1636228879324-8d9faf947576?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2127&q=80",
+            "https://images.unsplash.com/photo-1636228903781-35a277749bdc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2127&q=80",
+            "https://images.unsplash.com/photo-1636228997101-6880f283ef0f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2147&q=80",
+            "https://images.unsplash.com/photo-1636228886328-361b1472358f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2127&q=80"
+        ]
+    },
+    {
+        productId: 8,
+        productName: "Black",
+        productPrice: 49.99,
+        productImages: [
+            "https://images.unsplash.com/photo-1677530774319-d3406488543d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+            "https://images.unsplash.com/photo-1677531427899-c7a8a5a7f62e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+            "https://images.unsplash.com/photo-1677530792462-95ba5a55b120?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+        ]
+    },
+    {
+        productId: 9,
+        productName: "Blue",
+        productPrice: 59.99,
+        productImages: [
+            "https://images.unsplash.com/photo-1519345274080-93cee1c528eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1448&q=80",
+            "https://images.unsplash.com/photo-1519345510175-c43a1cb99912?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1448&q=80",
+            "https://images.unsplash.com/photo-1519135161701-e6f451f6d547?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1448&q=80"
         ]
     }
 ]
